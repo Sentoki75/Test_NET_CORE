332f6158eb40e3b473b1124c29eb3d4b0542727f
Jesús García Lahoz <jesus.garcialahoz@sage.com>
2020-04-19 23:03:12 +0200

    WIP 2 - Hello world

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Test_NET_CORE
+{
+    class Program
+    {
+        private const string HelloMessage = "Hello World!, there we go";
+
+        static void Main(string[] args) => Console.WriteLine(HelloMessage);
+    }
+}
