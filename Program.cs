ce22c0d4f6401c9f05b0c39ee0d86ef48c066b8d
Jesus Garcia Lahoz <jesus.garcialahoz@sage.com>
2020-04-25 12:28:41 -0400

    WIP 2 App and Release

--- Program.cs
+++ Program.cs
@@ -6,6 +6,17 @@ namespace Test_NET_CORE
     {
         private const string HelloMessage = "Hello World!, there we go";
 
-        static void Main(string[] args) => Console.WriteLine(HelloMessage);
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HelloMessage);
+            var counter = 0;
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
+            {
+                counter++;
+                Console.WriteLine($"Counter: {counter}");
+                System.Threading.Tasks.Task.Delay(1000).Wait();
+            }
+        }
     }
 }
